08ce78425c99cec050cc730024dc9213a595160e
Killian <kbaxter11@huskers.unl.edu>
2025-08-28 11:45:36 -0500

    Added major to items printed in terminal

--- hello.c
+++ hello.c
@@ -2,12 +2,14 @@
  * Author: Killian Baxter
  * Date: 2025/8/28
  *
- * Simple program that prints my name in the terminal
+ * Simple program that prints my name and major in the terminal
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
-    printf("Killian Baxter\n");
+    printf("Killian Baxter!\n");
+    printf("Computer Science\n");
+
 }
