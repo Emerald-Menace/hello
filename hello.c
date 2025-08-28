baaec8ac4aa05e92360d931a7621cd68fe4c791f
Killian <kbaxter11@huskers.unl.edu>
2025-08-28 11:22:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Killian Baxter
+ * Date: 2025/8/28
+ *
+ * Simple program that prints my name in the terminal
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("Killian Baxter\n");
+}
